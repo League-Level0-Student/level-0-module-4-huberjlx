ecc58fa30baad5bedc15e1b9605df89c0879ee79
root <root@c9f547ee0634>
2020-08-10 23:51:27 +0000

    Started module 4. stuck on converting string to int bc of decimal points

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,16 +12,25 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels =JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
+int nickelss = (int) (Integer.parseInt(nickels)*5);
 
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog("How many dimes do you have? ");
+int dimess = (int) (Integer.parseInt(dimes)*10);
 		// Ask the user how many quarters they have, and convert their answer
-
+String quarters = JOptionPane.showInputDialog("How many quarters do you have? ");
+int quarterss = (int) (Integer.parseInt(quarters)*25);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+int money = nickelss+dimess+quarterss;
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
+if( money % 100 < 10 ) {
+	JOptionPane.showMessageDialog(null, "You have $"+money/100 + ".0" + (money % 100 ));
+} else {
+	JOptionPane.showMessageDialog(null, "You have $"+money/100 + "." + (money % 100 ));	
+}
+
 
 	}
 }
---
+++ src/_01_double/_2_test_scores/testscores.java
@@ -0,0 +1,23 @@
+package _01_double._2_test_scores;
+
+import javax.swing.JOptionPane;
+
+public class testscores {
+
+	public static void main(String[] args) {
+	
+		String score = JOptionPane.showInputDialog("What did you get on your test? ");
+int response = Integer.parseInt(score);
+
+if (response >= 90) {
+	JOptionPane.showMessageDialog(null, "Wow, good job!");
+}
+else if(response >= 80) {
+	JOptionPane.showMessageDialog(null, "Try harder but not too shabby");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Study more, much more");
+}
+	}
+
+}
