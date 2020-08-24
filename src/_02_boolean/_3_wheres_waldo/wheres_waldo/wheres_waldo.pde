a039f34ecc8fade28552e8682c69ac0162ddb019
root <root@d17151f281e9>
2020-08-24 23:39:39 +0000

    Done with lesson 4

--- src/_02_boolean/_3_wheres_waldo/wheres_waldo/wheres_waldo.pde
+++ src/_02_boolean/_3_wheres_waldo/wheres_waldo/wheres_waldo.pde
@@ -8,9 +8,9 @@ AudioSample woohoo;
 void setup() {
   
   //Find a Where's Waldo picture and drop it into this sketch.     
-  PImage waldo = loadImage("waldo.jpg"); // Change this to match your file name.
+  PImage waldo = loadImage("waldo.png"); // Change this to match your file name.
   size(500, 500); // Change this to match the size of your waldo picture
-  image(waldo, 0, 0);
+  image(waldo, 500, 500);
 
   /*
     doh = minim.loadSample("homer-doh.wav"); 
@@ -20,7 +20,7 @@ void setup() {
 
 void draw() {
       // Use this print statement to find out the coordinates of Waldo when you press the mouse
-      // println("X: " + mouseX + " Y: " + mouseY); 
+       println("X: " + mouseX + " Y: " + mouseY); 
 
       // If the mouse is on Waldo, print “Waldo found!”
 
--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,25 +5,31 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+String characters = "abc";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-
+System.out.println(characters.charAt(2));
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(characters.length());
 		
+for(int i =0;i< characters.length(); i++) {
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
-		
+	System.out.println(characters.charAt(i));
+}
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+for(int j = 0; j <characters.length(); j++) {
+	if (j == 1) {
+		System.out.println("b is at index 1");
+	}
+	System.out.println(characters.charAt(j));
+}
 	}
 }
 
